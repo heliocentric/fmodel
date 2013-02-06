bb0e9fe6fbe3fb34ac7bd3fc00b938690e84ffec
Dylan Cochran <heliocentric@gmail.com>
2013-02-05 21:07:22 -0500

    * Add initial classes.

---
+++ fmodel/src/com/github/heliocentric/fmodel/Currency.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.heliocentric.fmodel;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Helio
+ */
+public class Currency {
+	
+}
---
+++ fmodel/src/com/github/heliocentric/fmodel/Viewer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.heliocentric.fmodel;
+
+/**
+ *
+ * @author Helio
+ */
+public class Viewer {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		Model main = new Model();
+	}
+}
