a477baccdd3a50b6c6b9676ec98d97e95755ca09
helio <helio@helio-Satellite-L305>
2013-02-08 12:14:42 -0500

    * Add Account class.

---
+++ fmodel/src/com/github/heliocentric/fmodel/Account.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.heliocentric.fmodel;
+
+/**
+ *
+ * @author helio
+ */
+public class Account {
+	private Currency _currency;
+	public Currency getCurrency() {
+		return this._currency;
+	}
+	public void setCurrency(Currency currency) {
+		this._currency = currency;
+	}
+}
