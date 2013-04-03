cc7896f1167fcff9098d637d877009a95c429444
helio <helio@helio-Satellite-L305>
2013-04-03 09:23:32 -0400

    * Refactor more classes

--- fmodel/src/com/github/heliocentric/fmodel/ModelClasses/Currency.java
+++ fmodel/src/com/github/heliocentric/fmodel/ModelClasses/Currency.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.github.heliocentric.fmodel.Model;
+package com.github.heliocentric.fmodel.ModelClasses;
 
 import java.math.BigDecimal;
 
---
+++ fmodel/src/com/github/heliocentric/fmodel/Runner.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.heliocentric.fmodel;
+
+/**
+ *
+ * @author helio
+ */
+public class Runner {
+	public static void main(String [ ] args)
+	{
+
+	}
+}
